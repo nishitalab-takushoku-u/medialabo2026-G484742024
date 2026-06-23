47fe7fb15697266e8eeaed4e95da60441b7df1ff
takahashi yuuma <248474@st.takushoku-u.ac.jp>
2026-06-23 12:43:43 +0900

    第3回の宿題

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,14 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+
+for(let room of campus.buildingD) {
+	console.log(room);
+}
+
+for(let r of gakka) {
+	console.log(r.name);
+}
+
+
